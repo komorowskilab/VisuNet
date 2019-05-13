034378356dd53ffb5f6d63575b1a523f1deec342
karsmo <32451075+karsmo@users.noreply.github.com>
2019-05-13 09:11:18 +0200

    Delete filterRules.R

--- filterRules.R
+++
@@ -1,9 +0,0 @@
-filterRules = function(RNobj, minAcc, minSupp, PercSupp){
-  if(length(RNobj$meanPERC_SUPP ) >0){
-    RNobjFiltr = RNobj[which(RNobj$meanAcc >= minAcc & RNobj$meanPERC_SUPP >= PercSupp),]
-  }else{
-    RNobjFiltr = RNobj[which(RNobj$meanAcc >= minAcc & RNobj$meanSupp >= minSupp),]
-  }
-  #print(head(RNobjFiltr))
-  return(RNobjFiltr)
-}
